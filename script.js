35e58b0cc0bf0da708869bdcf1aa782cd688aa4d
Jules <jules.nsenda@gmail.com>
2024-08-12 17:05:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,30 @@ function calculateWaterBill(consumption) {
     let bill = 0;
 
     if (consumption > 40) {
-        bill += (consumption - 40) * 57.36; // Calculate for >30-40 kl band
+        bill += (consumption - 40) * 57.36; // Calculate for >40 kl band
         consumption = 40;
     }
     if (consumption > 30) {
-        bill += (consumption - 30) * 52.44; // Calculate for >20-30 kl band
+        bill += (consumption - 30) * 52.44; // Calculate for >30-40 kl band
         consumption = 30;
     }
     if (consumption > 20) {
-        bill += (consumption - 20) * 37.95; // Calculate for >15-20 kl band
+        bill += (consumption - 20) * 37.95; // Calculate for >20-30 kl band
         consumption = 20;
     }
     if (consumption > 15) {
-        bill += (consumption - 15) * 27.07; // Calculate for >10-15 kl band
+        bill += (consumption - 15) * 27.07; // Calculate for >15-20 kl band
         consumption = 15;
+    }
+    if (consumption > 10) {
+        bill += (consumption - 10) * 25.94; // Calculate for >10-15 kl band
+        consumption = 10;
     }
     if (consumption > 6) {
         bill += (consumption - 6) * 25.94; // Calculate for >6-10 kl band
+        consumption = 6;
     }
     // The first 6 kl is free, so no charge for 0-6 kl.
-
     return bill;
 }
 
